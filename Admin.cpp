bf0e18fd57ae18596cdc5e2c6b5e001444ef8cd1
Aaathif <99630080+Aaathif@users.noreply.github.com>
2022-05-17 18:49:02 +0000

    s

--- Admin.cpp
+++ Admin.cpp
@@ -1,5 +1,5 @@
 // ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//IT21155352 Kamesh
 
 #include <iostream>
 #include<cstring>
--- Product.cpp
+++ Product.cpp
@@ -1,5 +1,5 @@
-// ConsoleApplication5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+//IT21156342 G.CPiyumal 
 
 / Dependency relationships
 
