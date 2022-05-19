4b05ac2565dcd3eff4e62b650267626f6e7a1d88
IT21154812 - Aathif <99630080+Aaathif@users.noreply.github.com>
2022-05-19 23:08:12 +0530

    Update Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -1,8 +1,3 @@
-
-//IT21156342 G.CPiyumal 
-
-/ Dependency relationships
-
 # include <iostream>
 # include <string>
 using namespace std;
@@ -37,9 +32,9 @@ public:
 
 	void displayDetails()
 	{
-		cout << " Product ID =" << productID << endl;
-		cout << " Product name =" << productName << endl;
-		cout << " Product Description = " << productDescription << endl;
+		cout << "Product ID =" << productID << endl;
+		cout << "Product name =" << productName << endl;
+		cout << "Product Description = " << productDescription << endl << endl;
 	}
 };
 
@@ -68,18 +63,10 @@ int main()
 	Product* P1 = new Product("P001", "Mugs", 150.00);
 	P1->setDescription("For camping");
 	Discount* D1 = new Discount();
+	P1->displayDetails();
 	D1->setDiscount(12.5);
 	D1->addProduct(5, P1);
 	D1->getDiscount();
+	
 	cin >> ch;
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
